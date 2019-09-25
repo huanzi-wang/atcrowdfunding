2847a8e74220d5ec81c4e9331337b9b8171efc1c
wanghuan <wanghuan@git.com>
2019-09-25 13:35:34 +0800

    第一次上传

---
+++ manager-api/src/main/java/com/tedu/atcrowdfunding/manager/dao/TestDao.java
@@ -0,0 +1,8 @@
+package com.tedu.atcrowdfunding.manager.dao;
+
+
+import java.util.Map;
+
+public interface TestDao {
+     void insert(Map map);
+}
---
+++ manager-api/src/main/java/com/tedu/atcrowdfunding/manager/service/TestService.java
@@ -0,0 +1,8 @@
+package com.tedu.atcrowdfunding.manager.service;
+
+
+public interface TestService {
+
+    public void insert();
+
+}
